f17f3b84ff57bac5c0a2c33d28738fabd56d6aab
cmkourtu <cmkourtu@gmail.com>
2025-06-06 12:42:05 -0500

    feat: improve objective page UX

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { initAnonUser } from './auth';
 import { ObjectivesPage } from './ObjectivesPage';
+import './style.css';
 
 initAnonUser();
 
