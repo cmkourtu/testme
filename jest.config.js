1ea4cf6c85cdffd77163ca37c0daa3e26f129b5d
cmkourtu <cmkourtu@gmail.com>
2025-06-09 11:28:40 -0500

    test: setup DeepSeek env

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/server/__tests__/setupEnv.ts'],
+  testPathIgnorePatterns: ['<rootDir>/server/__tests__/setupEnv.ts'],
   coverageReporters: ['json', 'lcov', 'text', 'clover', 'json-summary'],
 };
