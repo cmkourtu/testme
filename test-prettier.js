6606bf6ebc2ba3394eefecdc3f9437f29db1a20e
cmkourtu <cmkourtu@gmail.com>
2025-06-11 21:10:46 -0500

    adding pre commit hooks

--- test-prettier.js
+++
@@ -1,6 +0,0 @@
-// This file has intentional formatting issues to test the pre-commit hook
-const obj = { name: 'test', value: 123, nested: { a: 1, b: 2 } };
-function testFunction(param1, param2) {
-  console.log('This line has double quotes instead of single quotes');
-  return param1 + param2;
-}
